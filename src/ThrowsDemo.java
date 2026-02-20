6a7efab166ded50f34ba6491c875a0664b99d0c4
dragdab <139127963+dragdab@users.noreply.github.com>
2026-02-20 09:32:57 +0530

    Implement check method to throw IOException

--- src/ThrowsDemo.java
+++ src/ThrowsDemo.java
@@ -1,19 +1,35 @@
-import java.util.Scanner;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ThrowsDemo {
-    
-    // TODO: Define static method check(int n) that throws IOException
-    // If n < 0, throw new IOException()
-    // Else, print n
 
-   
-        // TODO: Read n
+    // Method that declares it throws a checked exception
+    public static void check(int n) throws IOException {
+        if (n < 0) {
+            // Explicitly throwing an IOException if n is negative
+            throw new IOException();
+        } else {
+            // If n is non-negative, simply print it
+            System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input Format: An integer
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+
+            try {
+                // Calling the method that might throw an exception
+                check(n);
+            } catch (IOException e) {
+                // Handling the specific checked exception
+                System.out.println("Caught: IO Exception");
+            }
+        }
         
-        // TODO: try {
-        //     call check(n)
-        // } catch (IOException e) {
-        //     Print "Caught: IO Exception"
-        // }
-    
+        scanner.close();
+    }
 }
